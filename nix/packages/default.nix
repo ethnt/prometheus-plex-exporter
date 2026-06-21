4b25bdb2175178c21347279c43034feaf1caab99
Ethan Turkeltaub <ethan@turkeltaub.dev>
2026-06-21 10:58:49 -0400

    Update dependencies, use custom `prometheus-plugs`

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -3,13 +3,13 @@ let
   beamPackages = beam27Packages.extend (self: super: { elixir = elixir_1_18; });
 in beamPackages.mixRelease rec {
   pname = "plex-exporter";
-  version = "dev";
+  version = "0.0.1";
   src = ../../.;
   mixEnv = "prod";
 
   mixFodDeps = beamPackages.fetchMixDeps {
     inherit pname version src mixEnv;
-    hash = "sha256-V2etU6jwmybTH7IUdusqvW+XXnYBVJ5ZTLRCOhpcS+A=";
+    hash = "sha256-dJUokAlVD1z2LDS1L+xia+bsp5ADv3po9pAcSGxcQiM=";
   };
 
   removeCookie = false;
